08af199b4925957fabb39c41a1a4eac7a179f8d6
YoungbeenNoh <youngbeennoh@gmail.com>
2020-10-04 11:52:09 +0900

    함수 중복제거

--- index.js
+++ index.js
@@ -1,56 +1,36 @@
 const todos = ['home', 'work', 'cleanup']
 
-function Createli(){
-    const liNode = document.createElement('li')
+function createTodo(text){
+    const li = document.createElement('li')
     const input = document.createElement('input')
     const button = document.createElement('button')
 
-    ul.append(liNode)
     li.append(input)
-    li.append(todos[i])
+    li.append(text)//인자값을 파라미터를 통해 받아서 li안으로 넣어서 보여준다.
     li.append(button)
     button.append('delete')
-}    
-const ul = document.querySelector('ul')
-for(let i = 0; i < todos.length; i++) {
-    Createli()
+    ul.append(li)
 
     input.type = "checkbox"
-    liNode.className = "todo"
+    li.className = "todo"
     input.className = "check"
     button.className = "delete"
 
-}
-const deletes = document.querySelectorAll('.delete')
-for(let i = 0; i < deletes.length; i++){
-    deletes[i].addEventListener('click',function(e){
-        removeTodo(e)
-        //e.target : 이벤트가 발생한 요소.
-    })
+    button.addEventListener('click',removeTodo)
 }
 
+const ul = document.querySelector('ul')
+for(let i = 0; i < todos.length; i++) {
+    createTodo(todos[i])
+}
 
-const add = document.querySelector('#add')
+const add = document.querySelector('#add')  
 add.addEventListener('click',function(){
     const input = document.querySelector('#todoValue')
     if(input.value === ""){
         alert('값을 입력하세요')
     }else{
-        const checkbox = document.createElement('input')
-        const listNode = document.querySelector('#list')
-        const button = document.createElement('button')
-        Createli()
-
-        checkbox.type = 'checkbox'
-        liNode.append(checkbox)
-        liNode.append(input.value)
-        liNode.append(button)
-        button.append('delete')
-        listNode.append(liNode)
-        
-        button.addEventListener('click',function(e){
-            removeTodo(e)
-        })
+        createTodo(input.value)
     }
 })
 
