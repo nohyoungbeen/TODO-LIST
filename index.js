e636c9df7a0f85c51221da79f67c8d60dfd12758
YoungbeenNoh <youngbeennoh@gmail.com>
2020-10-04 11:02:49 +0900

    함수 중복제거

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const todos = ['home', 'work', 'cleanup']
 
-const ul = document.querySelector('ul')
-for(let i = 0; i < todos.length; i++) {
-    const li = document.createElement('li')
+function Createli(){
+    const liNode = document.createElement('li')
     const input = document.createElement('input')
     const button = document.createElement('button')
 
-    input.type = "checkbox"
-    li.className = "todo"
-    input.className = "check"
-    button.className = "delete"
-
-    ul.append(li)
+    ul.append(liNode)
     li.append(input)
     li.append(todos[i])
     li.append(button)
     button.append('delete')
+}    
+const ul = document.querySelector('ul')
+for(let i = 0; i < todos.length; i++) {
+    Createli()
+
+    input.type = "checkbox"
+    liNode.className = "todo"
+    input.className = "check"
+    button.className = "delete"
+
 }
 const deletes = document.querySelectorAll('.delete')
 for(let i = 0; i < deletes.length; i++){
@@ -25,17 +29,17 @@ for(let i = 0; i < deletes.length; i++){
     })
 }
 
+
 const add = document.querySelector('#add')
 add.addEventListener('click',function(){
     const input = document.querySelector('#todoValue')
     if(input.value === ""){
         alert('값을 입력하세요')
     }else{
-
         const checkbox = document.createElement('input')
         const listNode = document.querySelector('#list')
         const button = document.createElement('button')
-        const liNode = document.createElement('li')
+        Createli()
 
         checkbox.type = 'checkbox'
         liNode.append(checkbox)
@@ -53,3 +57,4 @@ add.addEventListener('click',function(){
 function removeTodo(e){
     e.target.parentNode.remove()
 }
+
