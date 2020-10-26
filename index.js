454b0641ce9909ae4f3183bb1aebda5199246875
YoungbeenNoh <youngbeennoh@gmail.com>
2020-10-26 21:37:06 +0900

    todoList  ajax

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
 //할일 완료 : completed : true
 //미완료 : completed :false
-const todos = [
-    {title : '출근', completed : true},
-    {title :'공부', completed : true}
-]
+//https://jsonplaceholder.typicode.com/todos
+
+$.ajax({
+    url : "https://jsonplaceholder.typicode.com/users/1/todos" , 
+    type : "get",
+    dataType : "json",
+    success : function(response){
+        for(let i = 0; i < response.length; i ++){
+            createTodo(response[i].id, response[i].title, response[i].completed )
+            console.log(response);
+        }
+    },
+    fail : function(error){
+        console.log(error)
+    }
+})
 
 const add = document.querySelector('#add')
 
 const input = document.querySelector('#todoValue')
 
-for(i in todos){
-    createTodo(todos[i].title)
-}
-
-function createTodo(title){
+function createTodo(id, title , completed){
     const ul = document.querySelector('#list')
+    const li = document.createElement('li')
     const button = document.createElement('button')
     const checkbox = document.createElement('input')
     checkbox.type = "checkbox"
     ul.append(li) 
     li.append(checkbox)
+    li.append(id+"-")
     li.append(title)
     button.append("지우기")
     li.append(button)
     button.addEventListener('click', remove)
     checkbox.addEventListener('click', listColor)
+
+    if( completed == true){
+        checkbox.checked = true;
+        li.classList.add('completed');
+    }
 }
 
 function listColor(e){
